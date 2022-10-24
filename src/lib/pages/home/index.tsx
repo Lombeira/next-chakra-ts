3b76f161cf2318c1e712cd6398863da1feaa831e
Lombeira <glauco.lomenha@memed.com.br>
2022-10-24 14:11:43 -0300

    ci: changing tag

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { NextSeo } from "next-seo";
 
 import CTASection from "lib/components/samples/CTASection";
 import SomeImage from "lib/components/samples/SomeImage";
-import SomeText from "lib/components/samples/SomeText";
+// import SomeText from "lib/components/samples/SomeText";
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
       w="full"
     >
       <NextSeo title="Home" />
-      <SomeText />
+      {/* <SomeText /> */}
       <SomeImage />
       <CTASection />
     </Flex>
